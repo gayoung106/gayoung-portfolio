286715794a5b5a8c9fb7daa2f59a33bc28d81406
Gayoung Song <98731537+gayoung106@users.noreply.github.com>
2022-09-11 00:13:28 +0900

    recent work

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,17 +4,34 @@ import Menu from "./Menu";
 
 const Portfolio = () => {
   const [items, setItems] = useState(Menu);
+  const filterItem = (categoryItem) => {
+    const updatedItems = Menu.filter((curElem) => {
+      return curElem.category === categoryItem;
+    });
+
+    setItems(updatedItems);
+  };
 
   return (
     <section className="work container section" id="work">
       <h2 className="section__title">Recent Works</h2>
 
       <div className="work__filters">
-        <span className="work__item">Everything</span>
-        <span className="work__item">Creative</span>
-        <span className="work__item">Art</span>
-        <span className="work__item">Design</span>
-        <span className="work__item">Branding</span>
+        <span className="work__item" onClick={() => setItems(Menu)}>
+          Everything
+        </span>
+        <span className="work__item" onClick={() => filterItem("Creative")}>
+          Creative
+        </span>
+        <span className="work__item" onClick={() => filterItem("Art")}>
+          Art
+        </span>
+        <span className="work__item" onClick={() => filterItem("Design")}>
+          Design
+        </span>
+        <span className="work__item" onClick={() => filterItem("Branding")}>
+          Branding
+        </span>
       </div>
 
       <div className="work__container grid">
@@ -24,7 +41,14 @@ const Portfolio = () => {
             <div className="work__card" key={id}>
               <div className="work__thumbnail">
                 <img src={image} alt="" className="work__img" />
+                <div className="work__mask"></div>
               </div>
+
+              <span className="work__category">{category}</span>
+              <h3 className="work__title">{title}</h3>
+              <a href="#" className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
             </div>
           );
         })}
